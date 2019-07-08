51f4633fb9ea4b8ed04c01d21070e8caf1dab5dc
Thanya LeBlang <thanya2029@gmail.com>
2019-07-08 01:33:59 +0000

    show photos page

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -6,6 +6,10 @@ class PhotosController < ApplicationController
     redirect_to place_path(@place)
   end
 
+  def show
+    @place = Place.find(params[:place_id])
+  end
+
   private
 
   def photo_params
--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,7 @@ Rails.application.routes.draw do
   resources :places do
     resources :comments, only: :create
     resources :photos, only: :create
+    resources :photos, only: :show
   end
 
 end
